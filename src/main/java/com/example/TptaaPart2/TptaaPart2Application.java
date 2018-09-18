5ced69522cc98b6c97cac456a026a7f203873b36
Jeagles <Jeagles@Sybil>
2018-09-18 14:48:33 +0200

    Init maven project

---
+++ src/main/java/com/example/TptaaPart2/TptaaPart2Application.java
@@ -0,0 +1,12 @@
+package com.example.TptaaPart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TptaaPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TptaaPart2Application.class, args);
+	}
+}
